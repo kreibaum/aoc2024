3d57cc3b9bfe43adddc0d5bd8bc027fbc50a438b
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2024-12-09 19:39:55 +0100

    Solve day 6, part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Shared utility functions
+use std::collections::{HashMap, HashSet};
 use std::{fs::File, io::Read};
-use std::collections::HashMap;
 
 /// Read a file to a string.
 pub fn read_file(filename: &str) -> String {
@@ -11,11 +11,95 @@ pub fn read_file(filename: &str) -> String {
 }
 
 fn main() {
+    // Read information from day 6 file.
+    let file = read_file("day6");
+    let mut walls: HashSet<(i32, i32)> = HashSet::new();
+    let mut seen_positions: HashSet<(i32, i32)> = HashSet::new();
+
+    let mut guard_x: i32 = 0;
+    let mut guard_y: i32 = 0;
+
+    let mut w: i32 = 0;
+    let mut h: i32 = 0;
+
+    // Parse the file into a 2D array of booleans.
+    for (y, line) in file.lines().enumerate() {
+        h = (y + 1) as i32;
+        for (x, c) in line.chars().enumerate() {
+            w = (x + 1) as i32;
+            if c == '#' {
+                walls.insert((x as i32, y as i32));
+            } else if c == '^' {
+                guard_x = x as i32;
+                guard_y = y as i32;
+            }
+        }
+    }
+
+    // Print the area.
+    // print_area(&walls, w, h, guard_x, guard_y);
+
+    let mut direction = Direction { dx: 0, dy: -1 };
+
+    let mut steps_taken = 0;
+
+    // Let the guard move, as long as she is on the board
+    while 0 <= guard_x && guard_x < w && 0 <= guard_y && guard_y < h {
+        // Guards sees the square she is on
+        seen_positions.insert((guard_x, guard_y));
+        // Check if there is a wall in front of the guard.
+        let front = (guard_x + direction.dx, guard_y + direction.dy);
+        let is_wall = walls.contains(&front);
+        if is_wall {
+            direction = direction.turn_right();
+        } else {
+            guard_x += direction.dx;
+            guard_y += direction.dy;
+            steps_taken += 1;
+        }
+    }
+
+    println!("Size of the area: {}x{}", w, h);
+    println!("Steps taken: {}", steps_taken);
+    println!("Seen positions: {}", seen_positions.len());
+}
+
+struct Direction {
+    dx: i32,
+    dy: i32,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Direction {
+        // We use an upside down coordinate system, this is why the rotation is inverted.
+        Direction {
+            dx: -self.dy,
+            dy: self.dx,
+        }
+    }
+}
+
+fn print_area(area: &HashSet<(i32, i32)>, w: i32, h: i32, guard_x: i32, guard_y: i32) {
+    for y in 0..h {
+        for x in 0..w {
+            if area.contains(&(x as i32, y as i32)) {
+                print!("#");
+            } else if x == guard_x && y == guard_y {
+                print!("G");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn day2() {
     // Read file for day 2, then parse it line by line.
-    let mut parsed : Vec<Vec<i64>> = vec![];
+    let mut parsed: Vec<Vec<i64>> = vec![];
     let file = read_file("day2");
     for line in file.lines() {
-        let mut row : Vec<i64> = vec![];
+        let mut row: Vec<i64> = vec![];
         for num in line.split_whitespace() {
             row.push(num.parse().unwrap());
         }
@@ -35,11 +119,11 @@ fn main() {
 
     assert_eq!(safe_count, 321);
 
-
     let mut safe_count = 0;
     for row in parsed.iter() {
         println!("Row: {:?}", row);
-        let is_safe = is_ordered_after_removing_one(row, 1) || is_ordered_after_removing_one(row, -1);
+        let is_safe =
+            is_ordered_after_removing_one(row, 1) || is_ordered_after_removing_one(row, -1);
         if is_safe {
             safe_count += 1;
         }
@@ -55,7 +139,7 @@ fn is_strictly_ordered_max_n(row: &[i64], flip: i64) -> bool {
     find_ordering_problem(row, flip).is_none()
 }
 
-fn find_ordering_problem(row: &[i64], flip: i64)-> Option<usize> {
+fn find_ordering_problem(row: &[i64], flip: i64) -> Option<usize> {
     for i in 0..row.len() - 1 {
         let step = (row[i + 1] - row[i]) * flip;
         if step > MAX_STEP || step <= 0 {
@@ -94,8 +178,8 @@ fn day1() {
         let re = Regex::new(r"^(\d+) +(\d+)$").unwrap();
 
         let caps = re.captures(line).unwrap();
-        let left : i128 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right : i128 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let left: i128 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right: i128 = caps.get(2).unwrap().as_str().parse().unwrap();
 
         left_list.push(left);
         right_list.push(right);
@@ -119,7 +203,7 @@ fn day1() {
 
     // Part 2: Similarity score
     // Figure out how often each number appears in the right list.
-    let mut map : HashMap<i128, i128> = HashMap::new();
+    let mut map: HashMap<i128, i128> = HashMap::new();
     for &x in right_list.iter() {
         *map.entry(x).or_insert(0) += 1;
     }
@@ -127,7 +211,7 @@ fn day1() {
     // Find the similarity score
     let mut similarity_score = 0;
     for i in 0..left_list.len() {
-        similarity_score += map.get(&left_list[i]).cloned().unwrap_or( 0 ) * left_list[i];
+        similarity_score += map.get(&left_list[i]).cloned().unwrap_or(0) * left_list[i];
     }
     println!("Similarity score: {}", similarity_score);
 }
